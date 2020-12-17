cf474d5da9b879f63dcbf9a5f237ee09b8ed83e1
dain98 <dain98@gmail.com>
2020-12-17 10:03:50 +0000

    Creating a very simple base for Kogomi

---
+++ cogs/general.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+
+
+"""A simple cog example with simple commands. Showcased here are some check decorators, and the use of events in cogs.
+For a list of inbuilt checks:
+http://dischttp://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#checksordpy.readthedocs.io/en/rewrite/ext/commands/api.html#checks
+You could also create your own custom checks. Check out:
+https://github.com/Rapptz/discord.py/blob/master/discord/ext/commands/core.py#L689
+For a list of events:
+http://discordpy.readthedocs.io/en/rewrite/api.html#event-reference
+http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html#event-reference
+"""
+
+
+class General(commands.Cog):
+    """General"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(name='repeat', aliases=['copy', 'mimic'])
+    async def do_repeat(self, ctx, *, our_input: str):
+        """A simple command which repeats our input.
+        In rewrite Context is automatically passed to our commands as the first argument after self."""
+
+        await ctx.send(our_input)
+
+    @commands.command(name='add', aliases=['plus'])
+    @commands.guild_only()
+    async def do_addition(self, ctx, first: int, second: int):
+        """A simple command which does addition on two integer values."""
+
+        total = first + second
+        await ctx.send(f'The sum of **{first}** and **{second}**  is  **{total}**')
+
+    @commands.command(name='me')
+    @commands.is_owner()
+    async def only_me(self, ctx):
+        """A simple command which only responds to the owner of the bot."""
+
+        await ctx.send(f'Hello {ctx.author.mention}. This command can only be used by you!!')
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        """Event Listener which is called when a user is banned from the guild.
+        For this example I will keep things simple and just print some info.
+        Notice how because we are in a cog class we do not need to use @bot.event
+        For more information:
+        http://discordpy.readthedocs.io/en/rewrite/api.html#discord.on_member_ban
+        Check above for a list of events.
+        """
+
+        print(f'{user.name}-{user.id} was banned from {guild.name}-{guild.id}')
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(General(bot))
